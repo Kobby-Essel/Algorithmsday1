d7867364decd297f5201e147cac55abe135cff50
Kobby-Essel <esselpaakobina@gmail.com>
2023-06-10 20:00:07 -0700

    test

---
+++ luci.py
@@ -0,0 +1,8 @@
+first_name = "luci"
+last_name = "morningstar"
+
+full_name =first_name + " " + last_name
+
+first_name
+
+print(full_name.title())
---
+++ maths.py
@@ -0,0 +1,20 @@
+#A program to add numbers an display results
+
+def addit(a, b):
+    result = a + b
+    return result
+
+def sub(a,b):
+    result = a - b
+    print(result)
+
+def mul(a, b):
+    result = a*b
+    print(result)
+
+def div(a, b):
+    result = a/b
+    print(result)
+
+x = addit(20, 5)
+print(x)
